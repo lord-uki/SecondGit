b9afaba2b972c188b60a6ec6c34cdd2249988e59
lord-uki <lukasz.zawieja@gmail.com>
2017-03-28 15:30:41 +0100

    This is a test local repo-> global one

---
+++ HelloLocalGit/src/HelloLocalGit.java
@@ -0,0 +1,13 @@
+
+public class HelloLocalGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test");
+		System.out.println("Cos cos");
+
+	}
+
+}
